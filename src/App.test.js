4dc3390cd6f6815444880eb4a08f1b4f101ce59f
Anthony Hurtado <anhurtado@outlook.com>
2023-08-22 18:13:09 -0400

    feat: updated of unified testing of elements within the App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders toolbar with title React App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const toolbarTitle = screen.getByText(/React App/i);
+  expect(toolbarTitle).toBeInTheDocument();
+});
+
+test('renders select with first option All', () => {
+  render(<App />);
+  const selectElement = screen.getByRole('combobox');
+  const firstOption = screen.getByText(/All/i);
+  expect(selectElement.firstChild).toBe(firstOption);
+});
+
+test('renders table with appropriate headers', () => {
+  render(<App />);
+  const tableElement = screen.getByRole('table');
+  const fileNameHeader = screen.getByText('File Name');
+  const textHeader = screen.getByText('Text');
+  const numberHeader = screen.getByText('Number');
+  const hexHeader = screen.getByText('Hex');
+
+  expect(tableElement).toContainElement(fileNameHeader);
+  expect(tableElement).toContainElement(textHeader);
+  expect(tableElement).toContainElement(numberHeader);
+  expect(tableElement).toContainElement(hexHeader);
 });
